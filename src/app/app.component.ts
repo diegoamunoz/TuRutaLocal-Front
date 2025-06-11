acaaeac51471aa0c6579a03762719bef67090a63
Diego Munoz <diegoamunozb94@gmail.com>
2025-06-11 18:36:30 -0500

    se crearon los componentes de users y reservas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from "./components/layout/footer/footer.component";
+import { HeaderComponent } from "./components/layout/header/header.component";
+
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/public/home/home.component';
+import { LoginComponent } from './pages/public/login/login.component';
+import { RegisterComponent } from './pages/public/register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path:'', component: HomeComponent},
+    { path:'/login', component: LoginComponent  },
+    { path:'/register', component: RegisterComponent  },
+    { path: '**', redirectTo: 'home' , pathMatch: 'full' },
+    { path: '', redirectTo: 'home' , pathMatch: 'full' },
+];
