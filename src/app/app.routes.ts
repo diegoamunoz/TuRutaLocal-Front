76a6dfad012c2f172d598f661c22ac58fae60693
Diego Munoz <diegoamunozb94@gmail.com>
2025-06-11 18:46:27 -0500

    se crearon las nuevas entidades reservas y users

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,14 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/public/home/home.component';
 import { LoginComponent } from './pages/public/login/login.component';
 import { RegisterComponent } from './pages/public/register/register.component';
+import { ReservasComponent } from './pages/private/reservas/reservas.component';
+
 
 export const routes: Routes = [
     { path:'', component: HomeComponent},
-    { path:'/login', component: LoginComponent  },
-    { path:'/register', component: RegisterComponent  },
+    { path:'login', component: LoginComponent  },
+    { path:'register', component: RegisterComponent  },
+    { path:'reservas', component: ReservasComponent  },
     { path: '**', redirectTo: 'home' , pathMatch: 'full' },
     { path: '', redirectTo: 'home' , pathMatch: 'full' },
 ];
