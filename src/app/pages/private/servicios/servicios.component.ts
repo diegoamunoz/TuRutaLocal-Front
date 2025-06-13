13f160891f2dce8fd9636063122f855392fa6546
AGr <gonzalezrios205@gmail.com>
2025-06-12 20:46:41 -0500

    creacion de formulario especificando nombres e importando en el constructor

--- src/app/pages/private/servicios/servicios.component.ts
+++ src/app/pages/private/servicios/servicios.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-servicios',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './servicios.component.html',
   styleUrl: './servicios.component.css'
 })
 export class ServiciosComponent {
+  formData!:FormGroup ;
 
+  constructor(){
+    this.formData = new FormGroup({
+      name: new FormControl(),
+      description: new FormControl(),
+      price: new FormControl(),
+      category: new FormControl(),
+      state: new FormControl()
+    });
+  }
 }
