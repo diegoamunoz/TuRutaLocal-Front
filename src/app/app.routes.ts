464c926eabb9b4b8137eb2141ae5138fdc8945d1
Diego Munoz <diegoamunozb94@gmail.com>
2025-06-16 17:40:41 -0500

    se ajusta la rama users

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/public/home/home.component';
 import { LoginComponent } from './pages/public/login/login.component';
 import { RegisterComponent } from './pages/public/register/register.component';
-import { ReservasComponent } from './pages/private/reservas/reservas.component';
+// import { ReservasComponent } from './pages/private/reservas/reservas.component';
 import { UsersComponent } from './pages/private/users/users.component';
 import { UserNewForm } from './pages/private/users/new-form/new-form.component';
 
@@ -11,7 +11,7 @@ export const routes: Routes = [
     { path:'', component: HomeComponent},
     { path:'login', component: LoginComponent  },
     { path:'register', component: RegisterComponent  },
-    { path:'dashboard/reservas', component: ReservasComponent  },
+    // { path:'dashboard/reservas', component: ReservasComponent  },
     { path:'dashboard/usuarios', component: UsersComponent  },
     { path:'dashboard/usuarios/new', component: UserNewForm  },
     { path: '**', redirectTo: 'home' , pathMatch: 'full' },
