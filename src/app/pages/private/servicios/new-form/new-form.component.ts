1e26f5bd3249574da589bafd1bcdff0940094cff
AGr <gonzalezrios205@gmail.com>
2025-06-16 21:43:03 -0500

    creacion de services y conexion al backend

--- src/app/pages/private/servicios/new-form/new-form.component.ts
+++ src/app/pages/private/servicios/new-form/new-form.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-new-form',
@@ -12,15 +12,31 @@ formData!:FormGroup ;
 
   constructor(){
     this.formData = new FormGroup({
-      name: new FormControl(),
-      description: new FormControl(),
-      price: new FormControl(),
+      name: new FormControl('', [Validators.required, Validators.minLength(5), Validators.maxLength(50)]),
+      description: new FormControl( '', [Validators.required] ),
+      price: new FormControl(0, [Validators.min(0), Validators.max(100) ]  ),
       category: new FormControl(),
-      state: new FormControl()
+      urlImage:new FormControl(),
+      state: new FormControl('disponible', [])
     });
   }
 
   onSubmit() {
     console.log(this.formData.value);
+    console.log(
+      this.formData.valid,
+      this.formData.invalid,
+      this.formData.pristine,
+      this.formData.dirty,
+      this.formData.touched
+    );
+
+    if (this.formData.valid) {
+      console.log(this.formData.value);
+    }
+
+    this.formData.reset();
   }
+
+  
 }
