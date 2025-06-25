60ef757e31fb186d287c616231b36bcd33ccfba8
Diego Munoz <diegoamunozb94@gmail.com>
2025-06-24 21:02:41 -0500

    se actualiza el frontend de reservas y queda completa la rama con los campos conectados de service

--- src/app/pages/private/reservas/new-form/new-form.component.ts
+++ src/app/pages/private/reservas/new-form/new-form.component.ts
@@ -1,18 +1,21 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
+import { ServiciosService } from '../../../../servicios/servicios.service';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-reservas-new-form',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, JsonPipe],
   templateUrl: './new-form.component.html',
   styleUrls: ['./new-form.component.css']
 })
 
 export class ReservasNewForm {
-  formData!:FormGroup
-  constructor () {
+  formData!:FormGroup 
+  servicios:any=[]
+  constructor (private serviciosService:ServiciosService) {
 this.formData = new FormGroup({
-    nombreServicio: new FormControl('', [Validators.required]),
+    servicio: new FormControl('', [Validators.required]),
     fechaReserva: new FormControl('', [Validators.required]),
     cantidadPersonas: new FormControl('', [Validators.required]),
     estadoReserva: new FormControl('', [Validators.required]),
@@ -35,4 +38,23 @@ this.formData = new FormGroup({
       this.formData.reset(); // Limpiamos el formulario
     }
   }
-}
+  ngOnInit() {
+    this.serviciosService.getServicios().subscribe({
+      next: ( data ) => {
+        console.log( data );
+        this.servicios = data;
+      },
+      error: ( error ) => {
+        console.error( error );
+      },
+      complete: () => {
+        console.log( 'complete' );
+      }
+    });
+  }
+  ngOnDestroy() {
+    console.log( 'ngOnDestroy' );
+    }
+  }
+
+
---
+++ src/app/servicios/servicios.service.ts
@@ -0,0 +1,14 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ServiciosService {
+
+  constructor(private http:HttpClient) {}
+
+  getServicios() {
+  return this.http.get ('http://localhost:3000/api/servicios')
+ }
+}
