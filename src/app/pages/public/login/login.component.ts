30c5896377809d3c2f58930305f98ad22eefeee0
Diego Munoz <diegoamunozb94@gmail.com>
2025-07-01 21:06:10 -0500

    se terminó de crear el formulario reactivo y quedaron funcionando las validaciones, mostrando sus respectivos mensajes de error

--- src/app/pages/public/login/login.component.ts
+++ src/app/pages/public/login/login.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  formData!: FormGroup;
 
+  constructor() {
+    this.formData = new FormGroup({
+    username: new FormControl  ('', [ Validators.required, Validators.email ]),
+    password: new FormControl ('', [ Validators.required, Validators.minLength ( 6), 
+      Validators.maxLength(12) ])
+    });
+  }
+
+  onSubmit() {
+    if( this.formData.valid) {
+    console.log( this.formData.value);
+  }
+ 
+}
 }
