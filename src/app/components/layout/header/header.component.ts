3a490aedd57e34653e3f8ae489aab6b76732e7b5
AGr <gonzalezrios205@gmail.com>
2025-07-07 20:45:19 -0500

    [f]refinamiento del front end

--- src/app/components/layout/header/header.component.ts
+++ src/app/components/layout/header/header.component.ts
@@ -1,12 +1,8 @@
 import { Component } from '@angular/core';
-<<<<<<< HEAD
-import { RouterLink } from '@angular/router';
-=======
 import { Router, RouterLink } from '@angular/router';
 import { AuthService } from '../../../services/auth.service';
 import { Token } from '@angular/compiler';
 
->>>>>>> 22ee6898546bb01ab1988cee76438d57983556c8
 
 @Component({
   selector: 'app-header',
