6c33938f891a1f9aae09e7136041349cbbddefa9
Nicolas Campos <nicolascampos1621@gmail.com>
2025-07-07 20:54:16 -0500

    creacion de enlaces en contactos, destinos y nosotros en el header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,9 @@ import { RegisterComponent } from './pages/public/register/register.component';
 import { ReservasComponent } from './pages/private/reservas/reservas.component';
 import { destinosNewForm } from './pages/private/destinos/new-form/new-form.component';
 import { Resena } from './pages/private/resenas/new-form/new-form.component';
+import { ContactosComponent } from './pages/public/contactos/contactos.component';
+import { DestinosComponent } from './pages/public/destinos/destinos.component';
+import { NosotrosComponent } from './pages/public/nosotros/nosotros.component';
 
 
 
@@ -14,6 +17,9 @@ export const routes: Routes = [
     { path:'', component: HomeComponent},
     { path:'login', component: LoginComponent  },
     { path:'register', component: RegisterComponent  },
+    { path:'contactos', component: ContactosComponent},
+    { path:'destinos', component: DestinosComponent},
+    { path:'nosotros', component: NosotrosComponent},
     { path:'reservas', component: ReservasComponent  },
     { path:'dashboard/destinos/new-form', component: destinosNewForm},
     { path:'dashboard/resenas/new-form', component: Resena},    
---
+++ src/app/pages/public/destinos/destinos.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-destinos',
+  imports: [],
+  templateUrl: './destinos.component.html',
+  styleUrl: './destinos.component.css'
+})
+export class DestinosComponent {
+
+}
